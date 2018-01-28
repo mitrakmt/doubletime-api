76cf396b0a59672b982abc4faaf9c3de99b330d9
Michael Mitrakos <mike.mitrakos@verys.com>
2018-01-27 16:12:34 -0800

    fix: update db connection to add dialect

--- db/db.config.js
+++ db/db.config.js
@@ -1,7 +1,12 @@
 require('dotenv').config()
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize(process.env.DATABASE_URI)
+const sequelize = new Sequelize(process.env.DATABASE_URI, {
+  dialect: 'postgres'
+})
+// const sequelize = new Sequelize(process.env.DB_URL, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+//   dialect: 'postgres'
+// })
 
 sequelize.authenticate()
   .then(() => {
